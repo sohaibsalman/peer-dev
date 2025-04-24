93e14e34433eb57b084eda2b9acd187912912ec7
sohaibsalman <sohaibsalman10@gmail.com>
2025-04-24 23:46:16 +0500

    Ask a question UI

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,12 @@
-const AskQuestion = () => {
-  return <div>Ask Question</div>;
+import Question from "@/components/forms/Question";
+
+const AskQuestionPage = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <Question />
+    </div>
+  );
 };
 
-export default AskQuestion;
+export default AskQuestionPage;
---
+++ lib/validations.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const questionsSchema = z.object({
+  title: z.string().min(5).max(130),
+  explanation: z.string().min(100),
+  tags: z
+    .array(z.string().min(1).max(15))
+    .min(1, { message: "At least 1 tag is required." })
+    .max(3),
+});
